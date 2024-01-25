7fdfb261a3f38778979382964ca274fe07f56f62
Baxtiyor1990 <156651092+Baxtiyor1990@users.noreply.github.com>
2024-01-25 09:10:02 +0500

    Initial commit

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,48 +1,27 @@
 import unittest
 from utils import arrs
 
+class TestArrays(unittest.TestCase):
+    def test_get_existing_index(self):
+        array = [1, 2, 3, 4, 5]
+        result = arrs.get(array, 2)
+        self.assertEqual(result, 3)
 
-class TestGetFunction(unittest.TestCase):
+    def test_get_non_existing_index_with_default(self):
+        array = [1, 2, 3, 4, 5]
+        result = arrs.get(array, 10, default='not_found')
+        self.assertEqual(result, 'not_found')
 
-    def test_existing_index(self):
-        result = arrs.get([1, 2, 3], 1)
-        self.assertEqual(result, 2)
+    def test_my_slice_with_positive_indices(self):
+        array = [1, 2, 3, 4, 5]
+        result = arrs.my_slice(array, start=1, end=4)
+        self.assertEqual(result, [2, 3, 4])
 
-    def test_non_existing_index(self):
-        result = arrs.get([1, 2, 3], 5, default="Not Found")
-        self.assertEqual(result, "Not Found")
-
-    def test_negative_index(self):
-        result = arrs.get([1, 2, 3], -1, default="Not Found")
-        self.assertEqual(result, "Not Found")
-
-
-class TestMySliceFunction(unittest.TestCase):
-
-    def test_full_range(self):
-        result = arrs.my_slice([1, 2, 3, 4, 5])
-        self.assertEqual(result, [1, 2, 3, 4, 5])
-
-    def test_start_index(self):
-        result = arrs.my_slice([1, 2, 3, 4, 5], start=2)
+    def test_my_slice_with_negative_indices(self):
+        array = [1, 2, 3, 4, 5]
+        result = arrs.my_slice(array, start=-3, end=None)
         self.assertEqual(result, [3, 4, 5])
-
-    def test_end_index(self):
-        result = arrs.my_slice([1, 2, 3, 4, 5], end=2)
-        self.assertEqual(result, [1, 2])
-
-    def test_negative_start_index(self):
-        result = arrs.my_slice([1, 2, 3, 4, 5], start=-3)
-        self.assertEqual(result, [3, 4, 5])
-
-    def test_negative_end_index(self):
-        result = arrs.my_slice([1, 2, 3, 4, 5], end=-1)
-        self.assertEqual(result, [1, 2, 3, 4])
-
-    def test_empty_list(self):
-        result = arrs.my_slice([], start=1, end=3)
-        self.assertEqual(result, [])
-
 
 if __name__ == '__main__':
     unittest.main()
+
